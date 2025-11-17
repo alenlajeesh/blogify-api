cbab81a5db0573cb773932bcc881c15efd58392d
alenlajeesh <alenlajeesh@gmail.com>
2025-11-17 15:07:21 +0530

    Docs: Initial Server

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Blogify API\n');
-}).listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Hello from Blogify API\n');
 });
 
-// Graceful shutdown
-function shutdown(signal) {
-  console.log(`Received ${signal}. Closing server...`);
-  server.close(err => {
-    if (err) {
-      console.error('Error closing server:', err);
-      process.exit(1);
-    }
-    console.log('Server closed. BROOO');
-    process.exit(0);
-  });
-}
-
-process.on('SIGINT', () => shutdown('SIGINT'));
-process.on('SIGTERM', () => shutdown('SIGTERM'));
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
