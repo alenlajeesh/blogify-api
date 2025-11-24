653858aa5d346c80485e73e0d5bd391392624b5b
alenlajeesh <alenlajeesh@gmail.com>
2025-11-24 12:00:45 +0530

    Mistake

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
+// Import the new Post Router
+const postRouter = require("./routes/posts.routes");
 
-app.get('/', (req, res) => {
-  res.send('Hello from Blogify API\n');
+// Main welcome route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blogify API!");
 });
+
+// Mount the router at /api/v1/posts
+app.use("/api/v1/posts", postRouter);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}/`);
 });
+
