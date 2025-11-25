f840e347299ba702af6a8fea77abb084634c1c8f
alenlajeesh <alenlajeesh@gmail.com>
2025-11-25 15:15:56 +0530

    Modularized post routes using express.Router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-
 const postRouter = require("./routes/posts.routes");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Blogify API!");
 });
 
-
 app.use("/api/v1/posts", postRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
